21a035edb80d758854733c1928741e97b343427d
Max F. H <echecs972@gmail.com>
2026-08-16 18:09:02 +0000

    Configure settings for Render deployment

--- mysite_django/settings.py
+++ mysite_django/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# on PythonAnywhere, this file is /home/MFH/mysite_django/mysite_django/settings.py
+# so BASE_DIR is /home/MFH/, but this may change in GitHub / Render
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- Added MFH, 16.8.2026
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,7 +128,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-MEDIA_ROOT = '/home/MFH/mysite_django/media'
+# MEDIA_ROOT = '/home/MFH/mysite_django/media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/MFH/mysite_django/static'
+#STATIC_ROOT = '/home/MFH/mysite_django/static'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
